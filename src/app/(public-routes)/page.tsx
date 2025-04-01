90fa3af9b0ddac19a97b2126a64a1bb8fb4e8fa9
FILLIPE SANT'ANNA COTA <filipe_s_cota@estudante.sc.senai.br>
2025-04-01 19:36:32 -0300

    projeto

--- src/app/(public-routes)/page.tsx
+++ src/app/(public-routes)/page.tsx
@@ -1,5 +1,18 @@
+'use client'
+import "./style.css";
 export default function Home() {
   return (
-    <h1>Login</h1>
-  );
+    <div className="container">
+      <div className="form">
+        <img src="https://png.pngtree.com/png-vector/20190919/ourmid/pngtree-user-login-or-authenticate-icon-on-gray-background-flat-icon-ve-png-image_1742031.jpg" className="imagem" alt="imagem" />
+        <h1>Pagina de login</h1>
+        <input type="username" />
+        <input type="Password" />
+        <button className="button">
+          entrar
+        </button>
+      </div>
+
+    </div>
+  )
 }
