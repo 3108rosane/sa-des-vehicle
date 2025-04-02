6ba9f27b079ef36e438dd2df91a9e7105ae8d5e1
ROSANE MAIA BETINE <rosane_maia@estudante.sc.senai.br>
2025-04-01 21:41:53 -0300

    SA

--- src/app/(public-routes)/page.tsx
+++ src/app/(public-routes)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="container">
       <div className="form">
         <img src="https://png.pngtree.com/png-vector/20190919/ourmid/pngtree-user-login-or-authenticate-icon-on-gray-background-flat-icon-ve-png-image_1742031.jpg" className="imagem" alt="imagem" />
-        <h1>Pagina de login</h1>
+        <h1>Página de login</h1>
         <input type="username" />
         <input type="Password" />
         <button className="button">
