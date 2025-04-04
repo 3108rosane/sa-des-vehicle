493752a655bd659e679da6ee648a124321367f31
ROSANE MAIA BETINE <rosane_maia@estudante.sc.senai.br>
2025-04-03 21:55:36 -0300

    sa

--- src/components/mainMenu/index.tsx
+++ src/components/mainMenu/index.tsx
@@ -7,24 +7,24 @@ export default function MainMenu() {
 
 
    return (
-      <div className='header'>
+      <div className='first'>
          <Image src={logo} width={250} height={100} alt="logo" />
 
          <div>
             <div className='second'>
                <Personagem src='https://img.freepik.com/vetores-premium/perfil-de-avatar-de-homem-no-icone-redondo_24640-14044.jpg' />
 
-               <div>
+               <div className='third'>
                   <strong>fulano</strong>
                   <strong>fulano@gmail.com</strong>
                </div>
             </div>
             <div>
-               <span>home</span>
-               <span>manutenção</span>
-               <span>estoque</span>
-               <span>qualidade</span>
-               <span>produção</span>
+               <span className='four'>home</span>
+               <span className='four'>manutenção</span>
+               <span className='four'>estoque</span>
+               <span className='four'>qualidade</span>
+               <span className='four'>produção</span>
             </div>
          </div>
 
